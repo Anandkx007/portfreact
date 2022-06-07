1fb02a02950febaf5f8c6a8404cc3f0d509b346b
unknown <Anandkx007@gmail.com>
2022-06-07 15:04:05 +0530

    Updates

---
+++ precache-manifest.f841b602b514da6e45480331996938f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510db226e1094af0256a2abf3dc2fa03",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c9cc5bfa63b42b800c1",
+    "url": "/portfolio/static/css/main.5cbafb93.chunk.css"
+  },
+  {
+    "revision": "f125a787542f747a7369",
+    "url": "/portfolio/static/js/2.03d511f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.03d511f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9cc5bfa63b42b800c1",
+    "url": "/portfolio/static/js/main.0c7e35ee.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  }
+]);
